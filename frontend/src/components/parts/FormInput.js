05052cd7bd9ca99bd8f57cdca461b406e9d9eb8f
Sławomir Franczyk <slawomir.franczyk98@gmail.com>
2020-07-22 22:14:46 +0200

    datepicker added

--- frontend/src/components/parts/FormInput.js
+++ frontend/src/components/parts/FormInput.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
 import { Form } from 'react-bulma-components';
-import { useField } from 'formik';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.min.css';
+import { useField, useFormikContext } from 'formik';
+
 const { Input, Field, Control, Label, Help } = Form;
 
 export const FormInput = ({ label, ...props }) => {
 
-    const [field, meta] = useField(props);
-    const { touched, error } = meta; //todo display validation message
+    const [field] = useField(props);
+    const { setFieldValue, touched, errors } = useFormikContext();
+
+    const checkIfValid = () => touched[field.name] && !errors[field.name] ? 'is-success' : '';
 
     return (
         <Field>
             <Label htmlFor={field.name}>{label}</Label>
             <Control>
-                <Input id={field.name} {...field} {...props} className=""/>
+                {
+                    props.type === 'date'
+                        ? <DatePicker
+                            id={field.name}
+                            {...field}
+                            {...props}
+                            dateFormat="yyyy-MM-dd"
+                            placeholderText={props.placeholder}
+                            selected={field.value}
+                            onChange={val => { setFieldValue(field.name, val) }}
+                            minDate={new Date()}
+                            className={`input ${checkIfValid()}`}
+                        />
+                        : <Input id={field.name} {...field} {...props} className={checkIfValid()}/>
+                }
             </Control>
             {/*{validationMessage && <Help color={validationMessageType}>{message}</Help>}*/}
         </Field>
