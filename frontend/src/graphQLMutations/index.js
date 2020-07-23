0f9ff7182c5edee3cd6fa47d6ca42106814621a8
Sławomir Franczyk <slawomir.franczyk98@gmail.com>
2020-07-23 21:11:26 +0200

    code formatting

--- frontend/src/graphQLMutations/index.js
+++ frontend/src/graphQLMutations/index.js
@@ -1,5 +1,5 @@
 export const CREATE_EVENT_REGISTRATION_MUTATION = `mutation CreateEventRegistration($data: EventRegistrationCreateInput) {
-                                                createEventRegistration(data: $data) {
-                                                    id
-                                                }
-                                            }`;
+                                                        createEventRegistration(data: $data) {
+                                                            id
+                                                        }
+                                                    }`;
